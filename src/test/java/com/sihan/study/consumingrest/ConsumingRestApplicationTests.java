43cc591333c963b7645bcd61c76ea594706a3c5f
sihanwang <ocpwj@hotmail.com>
2020-04-08 15:56:36 +0800

    init

---
+++ src/test/java/com/sihan/study/consumingrest/ConsumingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.consumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
